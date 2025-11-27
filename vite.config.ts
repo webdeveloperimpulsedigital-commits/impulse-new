107d2de7ca430cc748df0e73f63e9d6414d8be31
shwetakhunte <shweta.khunte23@gmail.com>
2025-11-27 15:50:18 +0530

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwind from 'tailwindcss'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/impulse-new/',                 // keep your subfolder base
+  base: '/',                 // keep your subfolder base
   css: { postcss: { plugins: [tailwind()] } },
 
   // ✅ Ensure optional chaining & modern syntax are transpiled
