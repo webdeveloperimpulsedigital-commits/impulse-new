fb985fdc20f073d4b4d0d44761c6fae8183b00cf
shwetakhunte <shweta.khunte23@gmail.com>
2025-12-17 16:28:12 +0530

    footer social iocn updated

--- src/screens/Main/sections/FooterSection/FooterSection.tsx
+++ src/screens/Main/sections/FooterSection/FooterSection.tsx
@@ -205,7 +205,7 @@ export const FooterSection: React.FC = () => {
               className="actionBtn"
               aria-label="Chat on WhatsApp"
             >
-              <img src="/whatsapp.png" alt="WhatsApp" />
+              <img src="./WhatsApp.png" alt="WhatsApp" />
             </a>
 
             <a
@@ -213,7 +213,7 @@ export const FooterSection: React.FC = () => {
               className="actionBtn"
               aria-label="Call Impulse Digital"
             >
-              <img src="/footer-call.png" alt="Call" />
+              <img src="./call.png" alt="Call" />
             </a>
           </div>
         )}
