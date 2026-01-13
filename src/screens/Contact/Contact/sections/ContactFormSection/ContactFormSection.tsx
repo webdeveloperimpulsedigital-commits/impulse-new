3d6196ec31800db091d7b76bfc61b7937f59af07
shwetakhunte <shweta.khunte23@gmail.com>
2026-01-13 19:40:49 +0530

    contact form styling chnage

--- src/screens/Contact/Contact/sections/ContactFormSection/ContactFormSection.tsx
+++ src/screens/Contact/Contact/sections/ContactFormSection/ContactFormSection.tsx
@@ -43,15 +43,40 @@ export const ContactFormSection = () => {
   return (
     <section className="contact-section1 sm:pt-5 sm:pb-16 lg:pt-16 lg:pb-16 bg-white" id="contact-us-sec-border">
       <div className="max-w-[1280px] mx-auto px-0 lg:px-0 contact-container1">
-        {/* LEFT */}
+        {/* LEFT SECTION */}
         <div className="contact-left1">
+
           <h2 className="[font-family:'DM_Sans',Helvetica] font-normal text-[20px] md:text-[34px] leading-tight">
-            <span className="text-[#030019]">Let's put</span>
-            <br />
+            <span className="text-[#030019]">Let's put</span><br />
             <span className="font-bold text-[#030019] lg:text-[52px] md:text-[52px] sm:text-[20px]">
               Your Auto-fill to Use!
             </span>
           </h2>
+
+          <br />
+
+          <ul className="contact-list lg:pt-16 hidden lg:block md:block">
+            <img className="c-icon w-120" alt="Call Logo" src="call.png" />
+            <li>
+              <a href="tel:+919769285224" className="text-[#030019]">+91-9769285224</a>
+            </li>
+
+            <img className="c-icon w-120" alt="Email Logo" src="sms-1.png" />
+            <li>
+              <a href="mailto:collabs@theimpulsedigital.com">
+                collabs@theimpulsedigital.com
+              </a>
+            </li>
+
+            <img className="c-icon w-120" alt="location Logo" src="location.png" />
+            <li>
+              <address>
+                304 – 305, Chirag Infotech, Road No. 16/Z,<br />
+                Ambica Nagar, Wagle Industrial Estate,<br />
+                Thane, Mumbai 400604
+              </address>
+            </li>
+          </ul>
         </div>
 
         {/* RIGHT */}
@@ -138,6 +163,40 @@ export const ContactFormSection = () => {
           </div>
         </div>
       </div>
+
+         <style>{`
+      .w-120 {
+    width: 7%;
+    max-width: 10%;
+}
+        .contact-list {
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }
+
+        .contact-list li {
+          display: flex;
+          align-items: flex-start;
+          gap: 12px;
+          margin-bottom: 20px;
+          font-size: 16px;
+        }
+
+        .contact-list a,
+        .contact-list address {
+          color: #000000ff;
+          text-decoration: none;
+          line-height: 1.5;
+          font-size: 26px;
+          margin-bottom: 21px;
+          font-style: normal;
+        }
+
+        .contact-list a:hover {
+          color: white;
+        }
+      `}</style>
     </section>
   );
 };
