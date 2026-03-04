41331dad03902dee93bd699f5fbb68de6c579cd7
webdeveloperimpulsedigital-commits <webdeveloper.impulsedigital@gmail.com>
2026-03-04 15:55:56 +0530

    changes on navigation bar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -33,7 +33,7 @@ type HeaderProps = {
 
 const DEFAULT_ITEMS: NavItem[] = [
   { label: "Home", href: "/" },
-  { label: "About", href: "/about-us/" },
+  { label: "About us", href: "/about-us/" },
   {
     label: "Services",
     href: "/services/",
