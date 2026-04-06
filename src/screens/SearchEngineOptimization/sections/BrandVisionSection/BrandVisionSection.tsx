a85d0c9d64f93754fea0c73acff658852b1fb729
mahendarp <mahendarprajapati1995@gmail.com>
2026-04-06 18:51:31 +0530

    Update BrandVisionSection in SEO

--- src/screens/SearchEngineOptimization/sections/BrandVisionSection/BrandVisionSection.tsx
+++ src/screens/SearchEngineOptimization/sections/BrandVisionSection/BrandVisionSection.tsx
@@ -140,10 +140,10 @@ export const BrandVisionSection = (): JSX.Element => {
         {/* Title */}
         <div className="mb-6 lg:mb-8 pt-10">
           <h2 className="[font-family:'DM_Sans',Helvetica] text-[#030019] font-medium lg:text-[34px] sm:text-[16px] ">
-            The Smarter
+            The Smarter Way to Win Search
           </h2>
           <h2 className="[font-family:'DM_Sans',Helvetica] font-bold text-[#543d98] lg:text-[52px] sm:text-[26px] leading-tight ">
-               Way to Win Search
+               with an SEO Company in Thane
           </h2>
         </div>
 
